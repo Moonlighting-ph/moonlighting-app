f82d456e72764a9391bef69117e1f6872324f2e4
Cess <67199471+cessbub@users.noreply.github.com>
2025-02-28 20:49:33 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,11 @@ export default {
 		"./app/**/*.{ts,tsx}",
 		"./src/**/*.{ts,tsx}",
 	],
+	safelist: [
+		"h-[600px]",
+		"max-h-[350px]",
+		"overflow-hidden",
+	],
 	prefix: "",
 	theme: {
 		container: {
