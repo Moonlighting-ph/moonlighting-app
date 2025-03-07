0d6beb95361ef1412da93d26a65d9879f06e5b29
Cess <67199471+cessbub@users.noreply.github.com>
2025-03-07 23:18:26 +0800

    Update Moon.tsx

--- src/components/icons/Moon.tsx
+++ src/components/icons/Moon.tsx
@@ -3,23 +3,19 @@ import { SVGProps } from 'react';
 
 export function Moon(props: SVGProps<SVGSVGElement>) {
   return (
-    <div className="relative p-2 group">
-      <div className="absolute inset-0 rounded-full bg-primary opacity-20 group-hover:opacity-30 transition-opacity"></div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 32 32"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="h-7 w-7 text-primary relative z-10 group-hover:rotate-12 transition-transform duration-300"
-        {...props}
-      >
-        <path d="M16 4a8 8 0 0 0 12 12 12 12 0 1 1-12-12z" />
-      </svg>
-    </div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+    </svg>
   );
 }
